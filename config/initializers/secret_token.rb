0d304217f2c483bf04d6fa107260236ae3892208
Ryan Tremaine <ryan.trem@gmail.com>
2012-05-07 21:44:38 -0400

    new ror

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Summability::Application.config.secret_token = '083e7ac6438378dfb64050f44698864c4dfec924ad5c967348b01cb098c715433c5a9ba9b1a29f214cebdce056f539531e61968fdaf9c5141de4dc802876e8c3'
